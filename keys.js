b9244078bdc3f1bdef905fd776aeab1e645a37e1
Kathy Chen <kathychen.social@gmail.com>
2019-03-24 18:34:10 -0400

    updates to liri.js and keys.js

--- keys.js
+++ keys.js
@@ -4,3 +4,8 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bands = {
+  id: process.env.BANDSINTOWN_ID,
+  secret: process.env.BANDSINTOWN_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,3 +1,43 @@
-require("dotenv").config();
+/* --- Declare Constants and Variables --- */
+
+const dotenv = require("dotenv").config();
+const keys = require("./keys.js");
+
+var Spotify = require("node-spotify-api");
+var BandsInTown = require("node-bandsintown-api");
 
 var spotify = new Spotify(keys.spotify);
+var bands = new BandsInTown(keys.bands);
+var ombdKey = keys.ombdapi_key;
+
+var fs = require("fs");
+
+const firstCommand = process.argv[2];
+const secondCommand = process.argv[3];
+
+/* --- Functions --- */
+
+switch (firstCommand) {
+    case ("concert-this"):
+        getBands();
+        break;
+    case ("spotify-this-song"):
+        if (secondCommand) {
+            spotifyThisSong(secondCommand);
+        } else {
+            spotifyThisSong("Bennie And The Jets");
+        }
+        break;
+    case ('movie-this'):
+        if (secondCommand) {
+            omdb(secondCommand);
+        } else {
+            omdb("Interstellar");
+        }
+        break;
+    case ('do-what-it-says'):
+        doThing();
+        break;
+    default:
+        console.log('Error');
+};
