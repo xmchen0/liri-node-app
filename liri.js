f9fbc658553b5c58f76d319dadd75cf633c14680
Kathy Chen <kathychen.social@gmail.com>
2019-03-27 01:59:17 -0400

    added spotify and bands in town functionsto liri.js

--- liri.js
+++ liri.js
@@ -45,9 +45,9 @@ switch (command) {
 
     case "spotify-this-song":
         if (userInput) {
-            spotifyThisSong(userInput);
+            spotifySong(userInput);
         } else {
-            spotifyThisSong("The Sign");
+            spotifySong("The Sign Ace of Base");
         }
         break;
 
@@ -69,7 +69,7 @@ switch (command) {
 };
 
 
-// BANDS IN TOWN
+/* --- BANDS IN TOWN --- */
 // Command line: node liri.js concert-this <artist/band name here> 
 // Render the following information: 
 // 1. Name of the venue
@@ -77,8 +77,20 @@ switch (command) {
 // 3. Date of the Event (use moment to format this as "MM/DD/YYYY")
 // queryURL: https://rest.bandsintown.com/artists/circa%20survive/events?app_id=codingbootcamp
 
+var queryUrl = "https://rest.bandsintown.com/artists/" + bandQuery + "/events?app_id=codingbootcamp#";
 
-// SPOTIFY
+request(queryUrl, function (error, response, body) {
+    // If no error and request is successful
+    if (!error && response.statusCode === 200) {
+        var concertData = JSON.parse(body);
+        var momentDT = moment().format('L');
+        console.log("1. Venue: " + concertData[0].venue.name);
+        console.log("2. Location: " + concertData[0].venue.city + "," + concertData[0].venue.country);
+        console.log("3. Date: " + momentDT);
+    }
+});
+
+/* --- SPOTIFY --- */
 // Command line: node liri.js spotify-this-song '<song name here>'
 // Render the following information: 
 // 1. Artist(s)
@@ -86,8 +98,24 @@ switch (command) {
 // 3. A preview link of the song from Spotify
 // 4. The album that the song is from
 
+function spotifySong(song) {
+    spotify.search({ type: "track", query: song }, function (error, data) {
+        // If no error loop through song database
+        if (!error) {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                console.log("1. Artist: " + songData.artists[0].name);
+                console.log("2. Song: " + songData.name);
+                console.log("3. Preview URL: " + songData.preview_url);
+                console.log("4. Album: " + songData.album.name);
+            }
+        } else {
+            console.log('Error occurred.');
+        }
+    });
+}
 
-// OMDB
+/* --- OMDB --- */
 // Command line: node liri.js movie-this '<movie name here>'
 // Render the following information: 
 // 1. Title of the movie
@@ -103,6 +131,7 @@ function omdb(movie) {
     var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&apikey=trilogy';
 
     request(omdbURL, function (error, response, body) {
+        // If no error and request is successful, parse the data to become an object
         if (!error && response.statusCode == 200) {
             var body = JSON.parse(body);
             console.log("1. Title: " + body.Title);
@@ -116,6 +145,7 @@ function omdb(movie) {
         } else {
             console.log("Error occurred.")
         }
+        // If user does not type a movie, output data for the movie 'Mr. Nobody'
         if (movie === "Mr. Nobody") {
             console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
             console.log("It's on Netflix!");
@@ -123,16 +153,16 @@ function omdb(movie) {
     });
 };
 
-// DO WHAT IT SAYS
+/* --- DO WHAT IT SAYS --- */
 // Command line: node liri.js do-what-it-says
 // Render the following information: 
 // 1. LIRI take text inside random.txt to use it to call one of LIRI's commands
 // 2. Run spotify-this-song for "I Want it That Way"
 
 function doThing() {
+    // Run file system to grab random.text and spotify the song in the text file
     fs.readFile("random.txt", "utf8", function (error, data) {
         var text = data.split(",");
-        spotifyThisSong(text[1]);
+        spotifySong(text[1]);
     });
 };
-
